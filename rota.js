673e0bd1e39666149492ebe52d76ee0d901d2cb3
Lucas500260 <114434306+Lucas500260@users.noreply.github.com>
2022-12-26 22:37:44 -0300

    Create rota.js

---
+++ rota.js
@@ -0,0 +1,12 @@
+var expresso = require('expresso');
+var rota = express.Router();
+ 
+rota.get('/users', function(req, res) {
+    res.send(`Listar usuários`);
+});
+ 
+rota.post('/user', function(req, res) {
+    res.send(`Criar um usuário`);
+});
+ 
+module.exports = rota;
